9aaffb31cca67b7fa92b0586c6a0c9c9a4a184d1
Jayee <jayee.zheng@gmail.com>
2025-03-16 23:27:54 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,8 @@ export type Game = {
 };
 
 export type Round = {};
+
+export type CommentId = `t1_${string}`;
+export type UserId = `t2_${string}`;
+export type PostId = `t3_${string}`;
+export type SubredditId = `t5_${string}`;
