c7c6337bdf96121eeceb35946968ea5517a1d257
meiy112 <maggie.weng112@gmail.com>
2025-03-17 23:12:50 -0700

    Background assets

--- src/main.tsx
+++ src/main.tsx
@@ -86,7 +86,7 @@ Devvit.addCustomPostType({
     });
 
     return (
-      <zstack width="100%" height="100%">
+      <zstack width="100%" height="100%" alignment="center middle">
         <image
           url="bg-purple.png"
           width="100%"
@@ -96,12 +96,62 @@ Devvit.addCustomPostType({
           description="Background Image"
           resizeMode="fill"
         />
-
-        <vstack width="100%" height="100%" alignment="center middle">
-          <text size="large" weight="bold" color="white">
-            Content goes here
-          </text>
-        </vstack>
+        <hstack width="100%" height="100%" alignment="center middle">
+          <spacer width="15px" />
+          <image
+            url="assets.png"
+            width="100%"
+            height="100%"
+            imageWidth={500}
+            imageHeight={1000}
+            description="Assets Overlay"
+            resizeMode="fit"
+          />
+        </hstack>
+        <hstack height="100%" alignment="center middle">
+          <spacer width="15px" />
+          <image
+            url="assets-2.png"
+            width="100%"
+            height="100%"
+            imageWidth={500}
+            imageHeight={1000}
+            description="Assets Overlay"
+            resizeMode="fit"
+          />
+        </hstack>
+        <hstack width="440px" height="100%" alignment="center middle">
+          <vstack height="100%" width="60%" alignment="end top"></vstack>
+          <vstack width="40%" height="100%" alignment="start bottom">
+            <image
+              url="start-button.png"
+              width="95%"
+              height="14.5%"
+              imageWidth={500}
+              imageHeight={1000}
+              description="Start Drawing"
+            />
+            <image
+              url="tutorial-button.png"
+              width="95%"
+              height="14.5%"
+              imageWidth={500}
+              imageHeight={1000}
+              description="How to play"
+            />
+            <hstack height="59px" alignment="bottom center">
+              <spacer width="4px" />
+              <vstack
+                width="160px"
+                height="55px"
+                alignment="center middle"
+                backgroundColor="rgba(255, 255, 255, 0.25)"
+                cornerRadius="small"
+              ></vstack>
+            </hstack>
+            <spacer height="8px" />
+          </vstack>
+        </hstack>
       </zstack>
     );
   },
