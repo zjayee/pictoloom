358d3f6263801367485f3cec042e79100feb2168
meiy112 <maggie.weng112@gmail.com>
2025-03-19 17:57:04 -0700

    Assets folder move

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { Devvit } from "@devvit/public-api";
 import { appUpgradeSetup } from "./actions/installGame.js";
 import { dailyPostTrigger } from "./jobs/dailyPost.js";
 import { deleteTestPosts } from "./actions/deleteTestPosts.js";
-import GameScreen from "./components/GameScreen.js";
+import ChainDrawingMenu from "./components/ChainDrawingMenu.js";
 
 Devvit.configure({
   redditAPI: true,
@@ -33,7 +33,7 @@ Devvit.addTrigger(dailyPostTrigger);
 Devvit.addCustomPostType({
   name: "Web View Example",
   height: "tall",
-  render: GameScreen,
+  render: ChainDrawingMenu,
 });
 
 export default Devvit;
