e35642c0e4ce6a725fa4573aae5005a0513f24e6
meiy112 <maggie.weng112@gmail.com>
2025-03-27 15:45:25 -0700

    Merge

--- game/components/UpvoteDownvoteButton.tsx
+++ game/components/UpvoteDownvoteButton.tsx
@@ -56,7 +56,7 @@ export const UpvoteDownvoteButtons: React.FC<UpvoteDownvoteButtonsProps> = ({
 
   return (
     <div
-      className="flex h-[2em] items-center justify-center gap-x-[0.3em] rounded-full px-[0.5em] text-white transition-colors select-none"
+      className="z-10 flex h-[2em] items-center justify-center gap-x-[0.3em] rounded-full px-[0.5em] text-white transition-colors select-none"
       style={{ background: 'rgba(0, 0, 0, 0.25)' }}
     >
       {/* Up Arrow */}
