baff2d4d7daf7b730bb8895ea1eefa8f92bbee98
meiy112 <maggie.weng112@gmail.com>
2025-03-27 19:20:15 -0700

    Changed default page back to loading

--- game/hooks/usePage.tsx
+++ game/hooks/usePage.tsx
@@ -11,7 +11,7 @@ export const PageContextProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [page, setPage] = useState<Page>('reference');
+  const [page, setPage] = useState<Page>('loading');
 
   return (
     <PageUpdaterContext.Provider value={setPage}>
