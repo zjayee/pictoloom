9b3fa62bdf9a2a02eeba53ca7f521d68ba1f7450
Jayee <jayee.zheng@gmail.com>
2025-04-13 02:46:25 -0400

    Create supabase.ts

---
+++ src/storage/supabase.ts
@@ -0,0 +1,30 @@
+import { Devvit } from '@devvit/public-api';
+
+Devvit.addSettings([
+  {
+    // Name of the setting which is used to retrieve the setting value
+    name: 'supabase_url',
+    // This label is used to provide more information in the CLI
+    label: 'supabase api url',
+    // Type of the setting value
+    type: 'string',
+    // Marks a setting as sensitive info - all secrets are encrypted
+    isSecret: true,
+    // Defines the access scope
+    // app-scope ensures only developers can create/replace secrets via CLI
+    scope: 'app',
+  },
+  {
+    // Name of the setting which is used to retrieve the setting value
+    name: 'supabase_api_key',
+    // This label is used to provide more information in the CLI
+    label: 'supabase service key',
+    // Type of the setting value
+    type: 'string',
+    // Marks a setting as sensitive info - all secrets are encrypted
+    isSecret: true,
+    // Defines the access scope
+    // app-scope ensures only developers can create/replace secrets via CLI
+    scope: 'app',
+  },
+]);
